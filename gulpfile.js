48b48a6c8189e9222a3f6467be97fd764659f42a
TT <tt@a-kar.in>
2016-02-14 11:48:44 +0800

    fix sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 
+var fs = require('fs');
+var _ = require('lodash');
+
 var gulp = require('gulp');
 var clean = require('gulp-clean');
-var wrap = require("gulp-wrap");
 var uglify = require("gulp-uglify");
 var named = require('vinyl-named');
 var webpack = require('gulp-webpack');
 
 gulp.task('clean-bin', function() {
-  // TODO: copy file from tmp to bin
   return gulp.src('bin', {read: false})
-        .pipe(clean());
+    .pipe(clean());
 });
 
 gulp.task('clean-tmp', function() {
-  // TODO: copy file from tmp to bin
   return gulp.src('tmp', {read: false})
-        .pipe(clean());
+    .pipe(clean());
 });
 
 gulp.task('clean', ['clean-bin', 'clean-tmp']);
@@ -33,14 +33,27 @@ gulp.task('compile', ['lint', 'clean-tmp'], function() {
 gulp.task('pack', ['compile'], function(callback) {
   return gulp.src(['tmp/out/compile/*.js'])
     .pipe(named())
-    .pipe(webpack({ 'devtool': '#source-map' }))
+    .pipe(webpack({
+      'devtool': '#source-map',
+      'plugins': [
+        new webpack.webpack.BannerPlugin(
+          _.template(fs.readFileSync('header.js', 'utf8'))({
+            year: new Date().getFullYear()
+          }),
+          { raw: true }
+        )
+      ]
+    }))
     .pipe(gulp.dest('tmp/out/pack'));
 });
 
-gulp.task('build', ['pack', 'clean-bin'], function() {
-  return gulp.src(['tmp/out/pack/*.js'])
-    .pipe(wrap({ src: 'header.js' }))
+gulp.task('build-debug', ['pack'], function() {
+  return gulp.src(['tmp/out/pack/*.js', 'tmp/out/pack/*.map'])
     .pipe(gulp.dest('bin/debug'))
+});
+
+gulp.task('build', ['build-debug', 'pack', 'clean-bin'], function() {
+  return gulp.src(['bin/debug/*.js'])
     .pipe(uglify({
       mangle: false,
       preserveComments: function(node, comment) {
--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 /*!
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 shadow studio
+ * Copyright (c) ${year} Shadow Studio
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,5 +24,3 @@
  * Source code is accessed at:
  * <https://github.com/gzncland/AMTTT>
 */
-
-<%= contents %>
